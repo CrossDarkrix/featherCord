00390afc81139af90b88a20e5c232ecb499cc104
CrossDarkrix <crosseddarkrix@gmail.com>
2024-11-04 13:07:15 +0900

    Update setup-feathercord.sh

--- servers/setup-feathercord.sh
+++ servers/setup-feathercord.sh
@@ -6,4 +6,4 @@ echo "downloading background running command....."
 wget https://raw.githubusercontent.com/CrossDarkrix/featherCord/main/servers/_featherCord
 chmod +x _featherCord
 echo "running featherCord!"
-./_featherCord
+./featherCord
